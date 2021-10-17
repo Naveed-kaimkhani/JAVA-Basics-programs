aa000970e474a03c1521875e259ac601cfe7a184
Naveed kaimkhani <91529905+Naveed-kaimkhani@users.noreply.github.com>
2021-10-17 07:10:02 -0700

    Add files via upload

---
+++ File_handling.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class File_handling {
+    public static void main(String[] args)  //throws IOException
+    {
+        File myfile = new File("naveed.txt");
+        /*try {
+            myfile.createNewFile();    
+        } catch (IOException e) {
+            //TODO: handle exception
+            System.out.println("FIle not created");
+            System.out.println(e);
+        }
+        */
+        /*try {
+            FileWriter fileWriter = new FileWriter("naveed.txt");  
+            fileWriter.write("hehehheehehhe\nhahhahh");
+            fileWriter.close();  
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }*/
+
+        try {
+            Scanner sc= new Scanner(myfile);
+            while(sc.hasNextLine())
+            {
+                String line= sc.nextLine();
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e);
+        }
+
+
+    }
+}
